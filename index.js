89663d69ea5ea9861df7fb1a894041e5f2d384c6
Ashu Cha <ashucha@github.com>
2020-08-14 01:01:22 -0400

    Made navbar light with no transitions

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ $(() => {
   const navbar = $(".navbar");
   const competitionsOffset = $("#competitions").offset().top;
 
-  if (doc.scrollTop() >= competitionsOffset - 65) {
-    navbar.addClass("bg-light navbar-light");
-    navbar.removeClass("bg-translucent navbar-dark");
-  }
+  // if (doc.scrollTop() >= competitionsOffset - 65) {
+  //   navbar.addClass("bg-light navbar-light");
+  //   navbar.removeClass("bg-translucent navbar-dark");
+  // }
 
-  doc.on("scroll", () => {
-    if (doc.scrollTop() >= competitionsOffset - 65) {
-      navbar.addClass("bg-light navbar-light");
-      navbar.removeClass("bg-translucent navbar-dark");
-    } else {
-      navbar.addClass("bg-translucent navbar-dark");
-      navbar.removeClass("bg-light navbar-light");
-    }
-  });
+  // doc.on("scroll", () => {
+  //   if (doc.scrollTop() >= competitionsOffset - 65) {
+  //     navbar.addClass("bg-light navbar-light");
+  //     navbar.removeClass("bg-translucent navbar-dark");
+  //   } else {
+  //     navbar.addClass("bg-translucent navbar-dark");
+  //     navbar.removeClass("bg-light navbar-light");
+  //   }
+  // });
 });
