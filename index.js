c84a1258f78b1883177222a95863ebc84f7940c1
Ashu Cha <ashucha@github.com>
2020-08-14 00:56:04 -0400

    Added logo

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ $(() => {
   const navbar = $(".navbar");
   const competitionsOffset = $("#competitions").offset().top;
 
-  if (doc.scrollTop() >= competitionsOffset) {
-    navbar.addClass("bg-dark shadow");
-    navbar.removeClass("bg-translucent");
+  if (doc.scrollTop() >= competitionsOffset - 65) {
+    navbar.addClass("bg-light navbar-light");
+    navbar.removeClass("bg-translucent navbar-dark");
   }
 
   doc.on("scroll", () => {
-    if (doc.scrollTop() >= competitionsOffset) {
-      navbar.addClass("bg-dark");
-      navbar.removeClass("bg-translucent");
+    if (doc.scrollTop() >= competitionsOffset - 65) {
+      navbar.addClass("bg-light navbar-light");
+      navbar.removeClass("bg-translucent navbar-dark");
     } else {
-      navbar.addClass("bg-translucent");
-      navbar.removeClass("bg-dark");
+      navbar.addClass("bg-translucent navbar-dark");
+      navbar.removeClass("bg-light navbar-light");
     }
   });
 });
